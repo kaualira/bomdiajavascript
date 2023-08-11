d285d1adffe1c44cdb20c39f3055d75c95941ce9
kaualira <130668344+kaualira@users.noreply.github.com>
2023-08-11 16:29:50 -0300

    commit

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
-let hora = new Date ().getHours()-1;
-let saudacao = "" ;
+let hora = new Date().getHours();
+let saudacao = "";
 
-if (hora < 12) {
-    saudacao = "Bom dia!";
-} else if(hora >= 12 && hora <=18){
-    saudacao = "Boa tarde!";
-}else {
-    saudacao = "Boa noite!";
+if(hora < 12){
+    saudacao = "Bom dia";
+}else if(hora >= 12 && hora <= 18){
+    saudacao = "Boa tarde";
+}else{
+    saudacao = "Boa noite";
 }
+console.log(saudacao)
 
-console.log(saudacao);
-
-document.getElementById("resultado").innerHTI 
+document.getElementById("resultado").innerHTML = saudacao

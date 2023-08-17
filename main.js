325a9d43edf52371a0c0fc600e3757b79eee633f
kaualira <130668344+kaualira@users.noreply.github.com>
2023-08-17 09:44:47 -0300

    kl

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 let hora = new Date().getHours();
 let saudacao = "";
 
-if(hora < 12){
+if(hora >=6 && hora <= 12){
     saudacao = "Bom Dia";
-}else if(hora >= 12 && hora <= 18){
+}else if(hora >= 13 && hora <= 18){
     saudacao = "Boa Tarde";
 }else{
     saudacao = "Boa Noite";
